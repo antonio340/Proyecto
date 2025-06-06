090520558ff57a6724696faa9d427b967c597dc7
JorgeAlonsoVillanueva <101019519+JorgeAlonsoVillanueva@users.noreply.github.com>
2025-06-06 17:47:40 -0500

    borrado y cuenta nueva

--- ventas/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js' // Ruta completa
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
